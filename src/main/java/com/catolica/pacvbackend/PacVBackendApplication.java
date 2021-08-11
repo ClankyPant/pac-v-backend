851365effe00001eeb03cbab97a3b30fd6edcd92
Luis <luishenriquealbino@gmail.com>
2021-08-10 22:24:52 -0300

    init

---
+++ src/main/java/com/catolica/pacvbackend/PacVBackendApplication.java
@@ -0,0 +1,13 @@
+package com.catolica.pacvbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacVBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacVBackendApplication.class, args);
+	}
+
+}
