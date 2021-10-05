b5478a5b6e342a68f257664820f3116f7ee0f630
Luis <luishenriquealbino@gmail.com>
2021-10-05 18:58:25 -0300

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/com/pacvbackend/controller/UsuarioController.java
+++ src/main/java/com/pacvbackend/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.pacvbackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
